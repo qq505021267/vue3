6267f9b9ec3cb4e1d0021073a5a326cb8aa98026
Hins <505021267@qq.com>
2020-12-11 22:09:57 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="list">
+    <strong>编辑:</strong>
+    <CheckEditor
+      v-for="task in tasksRef"
+      :key="task.id"
+      v-model:title="task.title"
+      v-model="task.sell"
+    />
+  </div>
+  <div class="list">
+    <strong>销售中:</strong>
+    <div>
+      <template v-for="(it, i) in sellingListRef" :key="it.id">
+        <span>{{ i + 1 }}.</span>
+        <strong>{{ it.title }}</strong>
+      </template>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CheckEditor from "./components/CheckEditor.vue";
+import { ref, computed } from "vue";
 
 export default {
-  name: 'App',
+  setup() {
+    const tasksRef = ref([
+      {
+        id: 1,
+        sell: true,
+        title: "iphone12",
+      },
+      { id: 2, sell: false, title: "xiaomi" },
+      { id: 3, sell: true, title: "huawei" },
+      { id: 4, sell: true, title: "vivo" },
+    ]);
+
+    const sellingListRef = computed(() =>
+      tasksRef.value.filter((it) => it.sell)
+    );
+    return {
+      tasksRef,
+      sellingListRef,
+    };
+  },
+
   components: {
-    HelloWorld
-  }
-}
+    CheckEditor,
+  },
+};
 </script>
+
+<style  scoped>
+.container {
+  margin-top: 50px;
+  width: 880px;
+  margin: 50px auto;
+}
+
+.list {
+  display: flex;
+  margin: 1em 0;
+  align-items: center;
+}
+
+strong {
+  margin-right: 1em;
+}
+</style>
