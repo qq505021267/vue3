2eb12e1d87ccbf1fb61cbcb48a1bd4be1d7ae0fa
Hins <505021267@qq.com>
2021-01-11 19:26:00 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
+import { reactive, watchEffect, watch } from "vue";
+const state = reactive({
+  count: 0,
+});
+watchEffect(() => {
+  console.log("watchEffect", state.count);
+});
+watch(
+  () => state.count,
+  (count, oldCount) => {
+    console.log("watch", count, oldCount);
+  }
+);
+console.log("start");
+setTimeout(() => {
+  console.log("time out");
+  state.count++;
+  state.count++;
+});
+state.count++;
+state.count++;
 
-createApp(App).mount('#app')
+console.log("end");
